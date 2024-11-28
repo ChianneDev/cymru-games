f2135a0dd3baafb1ec428b7dbd01f0c148971b17
Chianne Coding <90045092+ChianneDev@users.noreply.github.com>
2024-11-28 10:09:24 +0000

    go-live website
    make a first live

---
+++ app.js
@@ -0,0 +1,18 @@
+function learnMore() {
+    window.open('learnmore.html');
+};
+function projectButton() {
+    window.open('product.html');
+};
+function aboutButton() {
+    window.open('aboutus.html');
+};
+function contactButton() {
+    window.open('contact.html');
+};
+function welshGov() {
+    window.open('https://media.service.gov.wales/news/game-on-welsh-companies-leading-the-way-at-the-game-developers-conference');
+};
+function featureButton(){
+    window.open('features.html');
+}
